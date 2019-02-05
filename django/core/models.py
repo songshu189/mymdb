60defd639e405b49e1e240687826e5a0cb2f80cf
songshu189 <songshu.yu@yahoo.com>
2019-02-05 13:36:35 -0500

    Add Person model and establish relationships between models

--- django/core/models.py
+++ django/core/models.py
@@ -1,5 +1,53 @@
 from django.db import models
 
+
+class PersonManager(models.Manager):
+    def all_with_prefetch_movies(self):
+        qs = self.get_queryset()
+        return qs.prefetch_related(
+            'directed',
+            'writing_credits',
+            'role_set__movie')
+
+
+class Person(models.Model):
+    first_name = models.CharField(
+        max_length=140)
+    last_name = models.CharField(
+        max_length=140)
+    born = models.DateField()
+    died = models.DateField(null=True,
+                            blank=True)
+
+    objects = PersonManager()
+
+    class Meta:
+        ordering = (
+            'last_name', 'first_name')
+
+    def __str__(self):
+        if self.died:
+            return '{}, {} ({}-{})'.format(
+                self.last_name,
+                self.first_name,
+                self.born,
+                self.died)
+        return '{}, {} ({})'.format(
+                self.last_name,
+                self.first_name,
+                self.born)
+
+
+class MovieManager(models.Manager):
+
+    def all_with_related_persons(self):
+        qs = self.get_queryset()
+        qs = qs.select_related('director')
+        qs = qs.prefetch_related(
+            'writers', 'actors')
+        return qs
+
+
 class Movie(models.Model):
     NOT_RATED = 0
     RATED_G = 1
@@ -23,6 +71,22 @@ class Movie(models.Model):
         models.PositiveIntegerField()
     website = models.URLField(
         blank=True)
+    director = models.ForeignKey(
+        to='Person',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='directed',
+        blank=True)
+    writers = models.ManyToManyField(
+        to='Person',
+        related_name='writing_credits',
+        blank=True)
+    actors = models.ManyToManyField(
+        to='Person',
+        through='Role',
+        related_name='acting_credits',
+        blank=True)
+    objects = MovieManager()
 
     class Meta:
         ordering = ('-year', 'title')
@@ -30,3 +94,17 @@ class Movie(models.Model):
     def __str__(self):
         return '{} ({})'.format(
             self.title, self.year)
+
+
+class Role(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.DO_NOTHING)
+    person = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=140)
+
+    def __str__(self):
+        return "{} {} {}".format(self.movie_id, self.person_id, self.name)
+
+    class Meta:
+        unique_together = ('movie',
+                           'person',
+                           'name')
--- django/core/views.py
+++ django/core/views.py
@@ -2,12 +2,20 @@ from django.views.generic import (
     ListView, DetailView,
 )
 
-from core.models import Movie
+from core.models import (
+	Movie, Person
+)
 
 
 class MovieDetail(DetailView):
     model = Movie
+    queryset = (
+    	Movie.objects
+    		.all_with_related_persons())
 
 class MovieList(ListView):
     model = Movie
     paginate_by = 10
+
+class PersonDetail(DetailView):
+	queryset = Person.objects.all_with_prefetch_movies()
